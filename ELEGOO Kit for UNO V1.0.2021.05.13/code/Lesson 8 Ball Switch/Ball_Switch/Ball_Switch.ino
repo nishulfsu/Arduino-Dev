c9638a18c362d5587db84c0f3115ed119c24d87c
Nishul Arora <nishul.arora@gmail.com>
2021-12-31 21:33:14 -0800

    Added green LED

--- ELEGOO Kit for UNO V1.0.2021.05.13/code/Lesson 8 Ball Switch/Ball_Switch/Ball_Switch.ino
+++ ELEGOO Kit for UNO V1.0.2021.05.13/code/Lesson 8 Ball Switch/Ball_Switch/Ball_Switch.ino
@@ -1,14 +1,18 @@
 //www.elegoo.com
 //2016.12.08
 /*****************************************/
-const int ledPin = 13;//the led attach to
-const int ballSwitchPin = 2; //the pin tilt ball switch is connected to
+
+//Adding an extra LED which will be turned on when no tilt is detected
+const int redledPin = 13;//the red led attach to
+const int greenledPin = 12;//the green led attach to
+const int ballswitchPin = 2; //the pin tilt ball switch is connected to
 
 void setup()
 { 
-  pinMode(ledPin,OUTPUT); //initialize the ledPin as an output
-  pinMode(ballSwitchPin,INPUT);
-  digitalWrite(ballSwitchPin, HIGH);
+  pinMode(redledPin,OUTPUT); //initialize the ledPin as an output
+  pinMode(greenledPin,OUTPUT); //initialize the ledPin as an output
+  pinMode(ballswitchPin,INPUT);
+  digitalWrite(ballswitchPin, HIGH);
 } 
 /******************************************/
 void loop() 
@@ -16,11 +20,14 @@ void loop()
   int digitalVal = digitalRead(2);
   if(HIGH == digitalVal)
   {
-    digitalWrite(ledPin,LOW);//turn the led off
+    digitalWrite(redledPin,HIGH);//turn the red led on
+    digitalWrite(greenledPin,LOW);//turn the green led off
+
   }
   else
   {
-    digitalWrite(ledPin,HIGH);//turn the led on 
+    digitalWrite(redledPin,LOW);//turn the led off
+    digitalWrite(greenledPin,HIGH);//turn the green led ON
   }
 }
 /**********************************************/
